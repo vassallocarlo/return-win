9f560a891473e36b650a40d89983cf5d6ef7aa72
carlo vassallo <carlo vassallo>
2016-03-22 17:12:08 +0100

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+//  google maps api initialization
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        scrollwheel: false,
+        zoom: 8
+    });
+}
+
+//  scripts
+$(function(){
+
+});
